80b658928e4fc597aeaa48e6f2f7fd1984cc853f
ALLEN-PC\acj30 <acj3017@gmail.com>
2023-03-18 13:20:33 -0500

    Fix new save button not appearing

--- src/game/gamepadui/gamepadui_savegame.cpp
+++ src/game/gamepadui/gamepadui_savegame.cpp
@@ -189,9 +189,6 @@ GamepadUISaveGamePanel::GamepadUISaveGamePanel( vgui::Panel* pParent, const char
 
     Activate();
 
-	if ( m_bIsSave )
-		m_Saves.AddToTail( SaveGameDescription_t{ "NewSavedGame", "", "", "#GameUI_NewSaveGame", "", "", "Current", NEW_SAVE_GAME_TIMESTAMP } );
-
     ScanSavedGames();
 
     if ( m_pSavePanels.Count() )
@@ -268,6 +265,9 @@ void GamepadUISaveGamePanel::ScanSavedGames()
 	m_Saves.Purge();
 	m_pSavePanels.PurgeAndDeleteElements();
 	m_pDeletePanels.PurgeAndDeleteElements();
+
+	if ( m_bIsSave )
+		m_Saves.AddToTail( SaveGameDescription_t{ "NewSavedGame", "", "", "#GameUI_NewSaveGame", "", "", "Current", NEW_SAVE_GAME_TIMESTAMP } );
 
 	// populate list box with all saved games on record:
 	char	szDirectory[_MAX_PATH];
